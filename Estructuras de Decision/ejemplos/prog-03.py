e927a9f586c66e96bc50c2d1986a82a0c445d608
Gerardo Beltrán <gerardobeltran@MacBook-Pro-de-Gerardo-213.local>
2020-09-14 12:46:29 -0700

    .

---
+++ Estructuras de Decision/ejemplos/prog-03.py
@@ -0,0 +1,8 @@
+numero = int(input("Ingrese un número: "))
+
+if (numero < 0):
+	print ("El numero es negativo")
+elif (numero == 0):
+	print ("El numero es cero")
+else:
+	print ("El numero es positivo")
---
+++ Estructuras de Decision/par-impar.py
@@ -0,0 +1,8 @@
+num = int(input())
+
+if (num % 2 == 0):
+        print ("Par")
+else:
+        print ("Impar")
+
+
